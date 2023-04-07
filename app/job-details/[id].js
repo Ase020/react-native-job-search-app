f9c6505eb3f908301ab76c33785bf48ad8e21623
felixNyalenda <felixasejunior@gmail.com>
2023-04-07 16:15:34 +0300

    final

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -44,9 +44,16 @@ const JobDetails = () => {
           />
         );
       case "About":
-        return <JobAbout info={data[0].job_description} />;
+        return (
+          <JobAbout info={data[0].job_description ?? ["No data provided"]} />
+        );
       case "Responsibilities":
-        break;
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
 
       default:
         break;
@@ -104,6 +111,13 @@ const JobDetails = () => {
             </View>
           )}
         </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
